005e9c8a2f062571dcb25bd207d3907e6174e132
Caio Vinicius Euzebio <caio.euzebio@mercadolivre.com>
2022-09-08 10:15:58 -0300

    Exercicios Java

---
+++ exercicios/src/funcamentos/AreaCircunferencia.java
@@ -0,0 +1,13 @@
+package funcamentos;
+
+public class AreaCircunferencia {
+    public static void main(String[] args) {
+        double raio = 3.4;
+        final double pi = 3.14159;
+
+        double area = pi*raio*raio;
+
+        System.out.println("A area é: "+area);
+        
+    }
+}
---
+++ exercicios/src/funcamentos/Temperatura.java
@@ -0,0 +1,17 @@
+package funcamentos;
+
+import javax.security.auth.Destroyable;
+
+public class Temperatura {
+    public static void main(String[] args) {
+        
+        final double FATOR = 5.0 / 9.0;
+        final double AJUSTE = 32;
+
+        double farenheit = 86.0;
+        double celsius = (farenheit - AJUSTE) * FATOR;
+        System.out.println("A temperatura em Celsius é: " + celsius);
+
+    }
+    
+}
