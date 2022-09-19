5f76768a02aee9520429ef528746c275c61e0c96
CaioEuzebio <caio.145@hotmail.com>
2022-09-18 23:13:51 -0300

    Adicionando metodo de formatação de data tipo String

--- exercicios/src/Classes/Data.java
+++ exercicios/src/Classes/Data.java
@@ -4,5 +4,9 @@ public class Data {
     int dia;
     int mes;
     int ano;
+
+    String obterDataFormatada () {
+        return (String) (dia + "/" + mes + "/" + ano);
+    }
     
 }
--- exercicios/src/Classes/DataTest.java
+++ exercicios/src/Classes/DataTest.java
@@ -8,7 +8,8 @@ public class DataTest {
         d1.mes = 12;
         d1.ano = 1996;
 
-        System.out.printf("%s/%s/%s",d1.dia,d1.mes,d1.ano);
+        System.out.printf("\n%s/%s/%s",d1.dia,d1.mes,d1.ano);
+        System.out.printf("\nA Data formatada é: %s",d1.obterDataFormatada());
     }
     
 }
